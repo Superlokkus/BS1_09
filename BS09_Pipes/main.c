0eb433b814e6ebc59de025c1d172ab25d301e8a0
Markus Klemm <markus@markusklemm.net>
2014-01-25 11:30:44 +0100

    Initial Commit

---
+++ BS09_Pipes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BS09_Pipes
+//
+//  Created by Markus Klemm on 25.01.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
