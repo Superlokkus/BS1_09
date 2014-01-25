7bed351ff9a6a427b0874f3c2513c4688a01c708
Markus Klemm <markus@markusklemm.net>
2014-01-25 15:57:47 +0100

    With rot13 child process

--- BS09_Pipes/main.c
+++ BS09_Pipes/main.c
@@ -8,6 +8,9 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define buffsize 128
 
@@ -25,23 +28,61 @@ static inline char rot13(char torotate)
 
 int main(int argc, const char * argv[])
 {
+    int parent2child[2];
+    if (pipe(parent2child)) {
+        perror("Pipe failed");
+        return EXIT_FAILURE;
+    }
     
-    char cbuff[buffsize];
-    
-    do {
-        fgets(cbuff, buffsize, stdin);
-        
-        char input[strlen(cbuff)+1];
-        strcpy(input, cbuff);
-        
-        int i;
-        for (i=0; i < strlen(input); i++) {
-            input[i] = rot13(input[i]);
+    pid_t mypid = fork();
+    if (mypid < 0) {
+        perror("Forking failed");
+        return EXIT_FAILURE;
+    }
+    else if (mypid == 0)
+    {
+        //Parent
+        if (close(parent2child[0])) {
+            perror("closing reading descrpt failed");
+            return EXIT_FAILURE;
         }
         
-        printf("%s",input);
+        char cbuff[buffsize];
         
-    } while (strstr(cbuff, "quit") == NULL);
+        do {
+            fgets(cbuff, buffsize, stdin);
+            write(parent2child[1], cbuff, sizeof(cbuff));
+            
+        } while (strstr(cbuff, "quit") == NULL);
+        
+        if (close(parent2child[1])) {
+            perror("Zombies!");
+            return EXIT_FAILURE;
+        }
+
+    }
+    else
+    {
+        //Child
+        if (close(parent2child[1])) {
+            perror("closing writeing descrpt failed");
+            return EXIT_FAILURE;
+        }
+        while (true) {
+            char input[buffsize];
+            if (read(parent2child[0], input, sizeof(input)) <= 0) {
+                close(parent2child[0]); //Failing isn't relevant,
+                return EXIT_SUCCESS;
+            }
+            int i;
+            for (i=0; i < strlen(input); i++) {
+                input[i] = rot13(input[i]);
+            }
+            
+            printf("%s",input);
+
+        }
+    }
     
     
     return 0;
